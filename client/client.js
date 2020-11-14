a9360050319af9cd724e0dec59dc9ad107000576
Joseph CG <JC-G>
2020-11-14 15:50:24 +0000

    Add ip box

--- client/client.js
+++ client/client.js
@@ -1,13 +1,26 @@
-const socket = new WebSocket('ws://10.0.18.24:8080', 'echo-protocol');
+var socket = undefined;
+var IP = "ws://10.0.18.24:8080";
+function connect(event) {
+    event.preventDefault();
+    IP = document.getElementById('IPbox').value;
+    console.log(IP);
+    socket = new WebSocket(IP, 'echo-protocol');
 
-socket.addEventListener('open',function(event) {
-    socket.send('Hello Server!');
-});
+    socket.addEventListener('open',function(event) {
+        socket.send('Hello Server!');
+    });
+    
+    socket.addEventListener('message',function(event) {
+        console.log('Message From Server', event.data);
+    });
+}
 
-socket.addEventListener('message',function(event) {
-    console.log('Message From Server', event.data);
-});
 
 function sendTest() {
     socket.send("Sent a test message");
 }
+window.addEventListener('load',function() {
+    document.getElementById('connectForm').addEventListener('submit',connect);
+    console.log("AAA");
+
+});
